faa7127f702fa3281ee11d3c85d48ba038bbac34
nicosignore <signore@matrix-gmbh.de>
2025-08-11 15:01:15 +0200

    initialized pygame

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    print("""
+    Screen width: 1280
+    Screen height: 720
+    """)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
+
+if __name__ == "__main__":
+    main()
